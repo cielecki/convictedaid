73765e1d8eb772ac55f4d41362b1e1ae34074e00
Maciej Cielecki <cielecki@gmail.com>
2014-08-20 14:17:27 +0200

    Reminder that siege weapons can not be attacked by ranged units.

--- js/invader_common.js
+++ js/invader_common.js
@@ -15,14 +15,16 @@ function createStandardSiegeMachines(SIEGE_ATTACK_TRAIT, siegeTraits) {
     var BRIDGE_TRAIT = {'name': 'Bridge', description: "Using a bridge eliminates all effects caused by a ditch/moat/sharpened stakes. Across every straight section of a ditch/moat/ sharpened stakes a maximum of 2 Bridges can be thrown. One Bridge is able to serve all of the invader’s troops. Having more Bridges on one section of a ditch/moat/ sharpened stakes means only that it’s going to be harder for the defender’s units to destroy all of the bridges. Note: A bridge is treated like an element placed in a Deployment Zone." };
     var SIEGE_TOWER_TRAIT = {'name': 'Attack', description: "A Siege Tower is extremely useful during a direct assault on a wall. A Combat Group attacking from a Siege Tower (we assign a Siege Tower to the strongest speci!c Combat Group at the  beginning of Step 10 - to select strongest Group we add ST of the front units + SU of the others) will completely ignore the defender’s forti!cation bonus during an assault. In addition, a defender’s Ranged Fire Strength is reduced by 1 any time they are targeting enemy units in a Foreground Zone of a forti!cation section with a Siege Tower . A Siege Tower moves like all other Melee Units." };
 
+    var CAN_NOT_BE_ATTACKED_BY_RANGED = {'name': 'Immune to ranged', description: "Ranged units can not attack siege weapons in step 1 and 6." };
+
     return [
-        { name: 'Battering ram', ST: 0, SU: 0, EN: 5, RST: 0, activateDuringSiegePhase: true, icon:'images/batteringram.png', size:'16', traits:[RAM_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Ballista', ST: 0, SU: 0, EN: 3, RST: '2xd3', activateDuringSiegePhase: true, icon:'images/ballista.png', size:'16', traits:[BALLISTA_TRAIT, SIEGE_ATTACK_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Catapult', ST: 0, SU: 0, EN: 3, RST: '3+d3', activateDuringSiegePhase: true, icon:'images/catapult.png', size:'16', traits:[CATAPULT_TRAIT, SIEGE_ATTACK_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Trebuchet', ST: 0, SU: 0, EN: 5, RST: '4+d3', activateDuringSiegePhase: true, icon:'images/trebuchet.png', size:'16', traits:[TREBUCHET_TRAIT, SIEGE_ATTACK_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Cover', ST: 0, SU: 0, EN: 6, RST: 0, activateDuringSiegePhase: true, icon:'images/cover.png', size:'16', traits:[COVER_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Bridge', ST: 0, SU: 0, EN: 4, RST: 0, activateDuringSiegePhase: true, icon:'images/bridge.png', size:'16', traits:[BRIDGE_TRAIT].concat(siegeTraits.slice(0))},
-        { name: 'Siege Tower', ST: 0, SU: 0, EN: 7, RST: 0, activateDuringSiegePhase: true, icon:'images/siegetower.png', size:'16', traits:[SIEGE_TOWER_TRAIT].concat(siegeTraits.slice(0))}
+        { name: 'Battering ram', ST: 0, SU: 0, EN: 5, RST: 0, activateDuringSiegePhase: true, icon:'images/batteringram.png', size:'16', traits:[RAM_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Ballista', ST: 0, SU: 0, EN: 3, RST: '2xd3', activateDuringSiegePhase: true, icon:'images/ballista.png', size:'16', traits:[BALLISTA_TRAIT, SIEGE_ATTACK_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Catapult', ST: 0, SU: 0, EN: 3, RST: '3+d3', activateDuringSiegePhase: true, icon:'images/catapult.png', size:'16', traits:[CATAPULT_TRAIT, SIEGE_ATTACK_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Trebuchet', ST: 0, SU: 0, EN: 5, RST: '4+d3', activateDuringSiegePhase: true, icon:'images/trebuchet.png', size:'16', traits:[TREBUCHET_TRAIT, SIEGE_ATTACK_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Cover', ST: 0, SU: 0, EN: 6, RST: 0, activateDuringSiegePhase: true, icon:'images/cover.png', size:'16', traits:[COVER_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Bridge', ST: 0, SU: 0, EN: 4, RST: 0, activateDuringSiegePhase: true, icon:'images/bridge.png', size:'16', traits:[BRIDGE_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))},
+        { name: 'Siege Tower', ST: 0, SU: 0, EN: 7, RST: 0, activateDuringSiegePhase: true, icon:'images/siegetower.png', size:'16', traits:[SIEGE_TOWER_TRAIT, CAN_NOT_BE_ATTACKED_BY_RANGED].concat(siegeTraits.slice(0))}
     ];
 }
 
